7331da6b106113291bc95be4eee3459767e19e75
Anil Kumar Behera <anilkumar567849@gmail.com>
2024-10-17 14:16:51 +0530

    add redis pub - sub architecture for scaling web sockets

--- server/dist/services/socket.js
+++ server/dist/services/socket.js
@@ -8,16 +8,36 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+const dotenv_1 = require("dotenv");
+(0, dotenv_1.config)();
 const socket_io_1 = require("socket.io");
+const ioredis_1 = __importDefault(require("ioredis"));
+const redis_pub = new ioredis_1.default({
+    port: parseInt(process.env.REDIS_PORT), // Redis port
+    host: process.env.REDIS_HOST, // Redis host
+    username: "default", // needs Redis >= 6
+    password: process.env.REDIS_PASSWORD,
+});
+const redis_sub = new ioredis_1.default({
+    port: parseInt(process.env.REDIS_PORT), // Redis port
+    host: process.env.REDIS_HOST, // Redis host
+    username: "default", // needs Redis >= 6
+    password: process.env.REDIS_PASSWORD,
+});
 class SocketService {
     constructor() {
+        this._io = null;
         this._io = new socket_io_1.Server({
             cors: {
                 allowedHeaders: ["*"],
                 origin: ['http://localhost:3000']
             }
         });
+        redis_sub.subscribe("redis:message");
     }
     initListeners() {
         const io = this.io;
@@ -26,13 +46,26 @@ class SocketService {
             // Listen for custom events
             socket.on("message", (message) => __awaiter(this, void 0, void 0, function* () {
                 console.log("server:msg", message);
-                socket.emit('message', message);
+                try {
+                    yield redis_pub.publish("redis:message", message);
+                }
+                catch (error) {
+                    console.log("Error", error);
+                }
             }));
             // Handle client disconnect
             socket.on("disconnect", () => {
                 console.log("Client disconnected:", socket.id);
             });
         });
+        redis_sub.on("message", (channel, message) => __awaiter(this, void 0, void 0, function* () {
+            switch (channel) {
+                case "redis:message":
+                    console.log('redis:sub:message', message);
+                    io === null || io === void 0 ? void 0 : io.emit("message", message);
+                    break;
+            }
+        }));
     }
     get io() {
         return this._io;
--- server/src/services/socket.ts
+++ server/src/services/socket.ts
@@ -1,37 +1,67 @@
+import { config } from 'dotenv';
+config();
 import { Server } from "socket.io";
+import Redis from "ioredis";
+
+const redis_pub = new Redis({
+    port: parseInt(process.env.REDIS_PORT as string) as number, // Redis port
+    host: process.env.REDIS_HOST as string, // Redis host
+    username: "default", // needs Redis >= 6
+    password: process.env.REDIS_PASSWORD as string,
+});
+const redis_sub = new Redis({
+    port: parseInt(process.env.REDIS_PORT as string) as number, // Redis port
+    host: process.env.REDIS_HOST as string, // Redis host
+    username: "default", // needs Redis >= 6
+    password: process.env.REDIS_PASSWORD as string,
+});
 
 
 export default class SocketService {
-    private _io: Server | null;
+    private _io: Server | null = null;
 
-    constructor(){
+    constructor() {
         this._io = new Server({
             cors: {
                 allowedHeaders: ["*"],
                 origin: ['http://localhost:3000']
             }
         })
+        redis_sub.subscribe("redis:message");
     }
 
-    public initListeners(){
+    public initListeners() {
         const io = this.io;
         io?.on("connection", (socket) => {
             console.log('user connected:', socket.id);
-            
+
             // Listen for custom events
             socket.on("message", async (message: string) => {
                 console.log("server:msg", message);
-                socket.emit('message', message)
+                try {
+                    await redis_pub.publish("redis:message", message);
+                } catch (error) {
+                    console.log("Error", error);
+                }
             });
-        
+
             // Handle client disconnect
             socket.on("disconnect", () => {
                 console.log("Client disconnected:", socket.id);
-            });  
+            });
+        })
+
+        redis_sub.on("message", async (channel, message) => {
+            switch (channel) {
+                case "redis:message":
+                    console.log('redis:sub:message', message);
+                    io?.emit("message", message);
+                    break;
+            }
         })
     }
 
-    get io(){
+    get io() {
         return this._io;
     }
 }
